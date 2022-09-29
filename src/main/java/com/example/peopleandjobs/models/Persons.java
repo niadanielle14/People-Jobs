50eccc10100d1c88cf7433f4441f44757e020290
Nia Watson <nia.d.watson@gmail.com>
2022-09-29 15:49:22 -0500

    feat. added Persons table to database using JPA

---
+++ src/main/java/com/example/peopleandjobs/models/Persons.java
@@ -0,0 +1,81 @@
+package com.example.peopleandjobs.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Persons {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private int age;
+    @Column(nullable = false, columnDefinition = "date")
+    private int dateJoined;
+    @Column(nullable = false, columnDefinition = "date")
+    private int dateUpdated;
+
+    public Persons(long id, String firstName, String lastName, int age, int dateJoined, int dateUpdated) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.dateJoined = dateJoined;
+        this.dateUpdated = dateUpdated;
+    }
+
+    public Persons() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getDateJoined() {
+        return dateJoined;
+    }
+
+    public void setDateJoined(int dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+
+    public int getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(int dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+}
