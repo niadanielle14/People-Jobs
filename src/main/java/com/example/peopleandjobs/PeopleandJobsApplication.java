72f9afbb2b07fdceca4bca2a34b66b916696fd67
Nia Watson <nia.d.watson@gmail.com>
2022-09-29 10:07:40 -0500

    Start of project. added readme and repository

---
+++ src/main/java/com/example/peopleandjobs/PeopleandJobsApplication.java
@@ -0,0 +1,13 @@
+package com.example.peopleandjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleandJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleandJobsApplication.class, args);
+    }
+
+}
