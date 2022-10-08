9422bc21c0500c2bb52614ecc445cd6f7d86eef3
Nia Watson <nia.d.watson@gmail.com>
2022-10-07 20:17:55 -0500

    feat. added seeder file

---
+++ seeder.sql
@@ -0,0 +1,9 @@
+USE ppl_jobs_db;
+
+insert into persons (first_name, last_name, age, job_title, date_joined, date_updated) values
+                                                                                           ('Francis', 'Costen', 73, 'Product Engineer', '2022-01-15', '2022-07-07'),
+                                                                                           ('Vin', 'Scoggans', 28, 'Environmental Specialist', '2022-05-04', '2022-03-01'),
+                                                                                           ('Dollie', 'Garratt', 86, 'Engineer III', '2021-11-03', '2022-04-23'),
+                                                                                           ('Jareb', 'Banat', 4, 'Assistant Manager', '2022-07-19', '2022-05-06'),
+                                                                                           ('Irita', 'MacEntee', 33, 'Data Coordiator', '2022-05-05', '2022-02-22');
+
