c50fd51d0281f90313fc2cb167c2025518d0f197
Nia Watson <nia.d.watson@gmail.com>
2022-10-16 21:21:32 -0500

    added depenedency injection and Views

--- src/main/java/com/example/peopleandjobs/controllers/personsController.java
+++ src/main/java/com/example/peopleandjobs/controllers/personsController.java
@@ -1,13 +1,27 @@
 package com.example.peopleandjobs.controllers;
 
+import com.example.peopleandjobs.repositories.JobsRepo;
+import com.example.peopleandjobs.repositories.PersonsRepo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class personsController {
+
+    private final PersonsRepo personsDao;
+    private final JobsRepo jobsDao;
+
+    public personsController(PersonsRepo personsDao, JobsRepo jobsDao) {
+        this.personsDao = personsDao;
+        this.jobsDao = jobsDao;
+    }
+
     @GetMapping("/index")
-    public String index() { return "persons/index";
+    public String index(Model model) {
+        model.addAttribute("persons", personsDao.findAll());
+        return "persons/index";
     }
 
 }
